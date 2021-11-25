5168c3a08aedae0b22cbdaad364636f4a49b6a62
yuminhwan <ymh7532@icloud.com>
2021-11-26 01:52:27 +0900

    feat: 플레이어가 숫자 입력하는 기능 구현

---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Player {
+
+	private Number numbers;
+
+	public void inputNumbers() {
+		String inputNumbers = Console.readLine();
+	}
+
+	public Number getNumbers() {
+		return numbers;
+	}
+
+}
